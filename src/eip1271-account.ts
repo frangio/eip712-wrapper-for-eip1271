82681366163d920fd832aa42e87ed3cdffc840ac
Francisco Giordano <fg@frang.io>
2023-11-01 00:04:25 +0000

    rename DomainSeparator -> DomainHash

--- src/eip1271-account.ts
+++ src/eip1271-account.ts
@@ -10,7 +10,7 @@ const wrapperTypeName = 'EIP1271Wrapper';
 
 export function eip1271Account(signerKey: Hex, address: Hex, chainId?: number) {
   const accountDomain = { verifyingContract: address, chainId };
-  const accountDomainSeparator = hashDomain(accountDomain);
+  const accountDomainHash = hashDomain(accountDomain);
 
   const signerAccount = privateKeyToAccount(signerKey);
 
@@ -51,7 +51,7 @@ export function eip1271Account(signerKey: Hex, address: Hex, chainId?: number) {
         const messageDomain = typedData.domain;
         assert(messageDomain !== undefined, `Duplicate type definition for ${wrapperTypeName}`);
 
-        const messageDomainSeparator = hashDomain(messageDomain);
+        const messageDomainHash = hashDomain(messageDomain);
         const contentsHash = hashStruct({ data: typedData.message, primaryType: typedData.primaryType, types });
 
         const wrappedTypedData: TypedDataDefinition = {
@@ -66,14 +66,14 @@ export function eip1271Account(signerKey: Hex, address: Hex, chainId?: number) {
           },
           message: {
             contents: typedData.message,
-            message: concat(['0x1901', messageDomainSeparator, contentsHash]),
+            message: concat(['0x1901', messageDomainHash, contentsHash]),
           },
         };
 
         const rawCompactSignature = await signTypedDataCompact(wrappedTypedData);
         const typeHash = hashType(wrappedTypedData);
 
-        return concat([rawCompactSignature, typeHash, messageDomainSeparator, contentsHash]);
+        return concat([rawCompactSignature, typeHash, messageDomainHash, contentsHash]);
       },
 
       async signTransaction() {
@@ -90,16 +90,16 @@ export function eip1271Account(signerKey: Hex, address: Hex, chainId?: number) {
 
         switch (size(signature)) {
           case 96: {
-            const hash = keccak256(concat(['0x1901', accountDomainSeparator, keccak256(concat([typeHash, messageHash]))]));
+            const hash = keccak256(concat(['0x1901', accountDomainHash, keccak256(concat([typeHash, messageHash]))]));
             const recovered = await recoverAddress({ hash, signature: rawSignature });
             return recovered === signerAccount.address;
           }
 
           case 160: {
-            const messageDomainSeparator = slice(signature, 96, 128, { strict: true });
+            const messageDomainHash = slice(signature, 96, 128, { strict: true });
             const contentsHash = slice(signature, 128, 160, { strict: true });
-            const hash = keccak256(concat(['0x1901', accountDomainSeparator, keccak256(concat([typeHash, contentsHash, messageHash]))]));
-            const messageHash2 = keccak256(concat(['0x1901', messageDomainSeparator, contentsHash]));
+            const hash = keccak256(concat(['0x1901', accountDomainHash, keccak256(concat([typeHash, contentsHash, messageHash]))]));
+            const messageHash2 = keccak256(concat(['0x1901', messageDomainHash, contentsHash]));
             const recovered = await recoverAddress({ hash, signature: rawSignature });
             return messageHash === messageHash2 && recovered === signerAccount.address;
           }
